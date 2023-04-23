6a47e6a85b44b03f58abe97bffe40c73ac11ed52
EternalFlam3-14 <Cognitivelybrad@outlook.com>
2023-04-22 23:06:16 -0400

    Build MapFactory File Input

--- include/engine/item/factory/mapfactory/headers/mapfactory.h
+++ include/engine/item/factory/mapfactory/headers/mapfactory.h
@@ -3,6 +3,8 @@
 
 #include <map>
 #include <string>
+#include <vector>
+#include <Cereal.h>
 
 class MapFactory_C
 {
@@ -10,14 +12,23 @@ public:
 
     MapFactory_C() {;};
 
-    std::map <std::string, std::string> load_Map();
-
+    bool load();
 
 
 private:
 
+    // Returns the ItemNames and ItemTypes vectors from the database as a 2d vector of strings
+    std::vector<std::vector<std::string>> load_Arrays(Cereal::Object* Items);
+
+    // Returns the ItemName/ItemType map
+    std::map <std::string, std::string> build_Map(Cereal::Object* TypeMap, std::vector<std::vector<std::string>> ItemArrays);
+
+    // Internal check method used to verify the map isn't missing anything
+    bool has_Error(std::map <std::string, std::string> ItemTypeMap, std::vector<std::vector<std::string>> ItemArrays);
+
     std::string Filename = "config/item.db";
 
+    std::map <std::string, std::string> TypeMap;
 
 };
 #endif
--- include/engine/item/factory/mapfactory/src/mapfactory.cpp
+++ include/engine/item/factory/mapfactory/src/mapfactory.cpp
@@ -1,10 +1,99 @@
 #include "mapfactory.h"
+#include <algorithm>
 
-#include <Cereal.h>
-
-std::map <std::string, std::string> MapFactory_C::load_Map()
+bool MapFactory_C::load()
 {
+    // Read file into buffer
+    Cereal::Buffer buf(1024);
+    buf.readFile(Filename);
 
+    // Read buffer information into the database
+    Cereal::Database* db = new Cereal::Database;
+    db->read(buf);
+
+    // Grab the Items object out of the database
+    Cereal::Object* ItemsObject = db->getObject("Items");
+
+    // Grab the TypeMap object from the database
+    Cereal::Object* TypeMapObject = db->getObject("TypeMap");
+
+    // Contains ItemNames and Types, [0] and [1] respectively
+    auto ItemArrays = load_Arrays(ItemsObject);
+
+    // The map is loaded in from the Cereal Object
+    TypeMap = build_Map(TypeMapObject, ItemArrays);
+
+    delete db; // Deletes all Cereal instances
+
+    return has_Error(TypeMap, ItemArrays);
 }
 
+std::vector<std::vector<std::string>> MapFactory_C::load_Arrays(Cereal::Object* Items)
+{
+    // Gather arrays from object
+    std::vector<Cereal::Array*> ItemArrays = Items->getArrays(); 
+    
+    // Seperate arrays into respective variables
+    std::vector<std::vector<std::string>> ItemInfo;
 
+    ItemInfo.push_back(ItemArrays[0]->getArray<std::string>());
+
+    ItemInfo.push_back(ItemArrays[1]->getArray<std::string>());
+
+    return ItemInfo;
+}
+
+std::map<std::string, std::string> MapFactory_C::build_Map(Cereal::Object *TypeMap, std::vector<std::vector<std::string>> ItemArrays)
+{
+
+    std::map <std::string, std::string> ItemTypeMap;
+
+    for (auto Item : ItemArrays[0])
+    {
+        // Item's paired type
+        std::string Type;
+        
+        // Fetch the string under the name of the current item
+        Type = TypeMap->getField(Item)->getValue<std::string>();
+
+        // Error if the paired type cannot be found in the Item Type array
+        if (std::find(ItemArrays[1].begin(), ItemArrays[1].end(), Type) == ItemArrays[1].end())
+        {
+            continue;
+        }
+
+        // Store the pair into the map with the item as the key
+        ItemTypeMap.emplace(std::pair<std::string, std::string> (Item, Type));
+    }
+    return ItemTypeMap;
+}
+
+bool MapFactory_C::has_Error(std::map <std::string, std::string> ItemTypeMap, std::vector<std::vector<std::string>> ItemArrays)
+{
+    std::vector<std::string> MissingItems, MissingTypes;
+
+    // First check that all items exist in the Map
+    for (auto Item : ItemArrays[0])
+    {
+        if(ItemTypeMap.find(Item) == ItemTypeMap.end())
+        {
+            MissingItems.push_back(Item);
+        }
+    }
+
+    // Then, check that all types exist in the Map
+    for (auto Type : ItemArrays[1])
+    {
+        if(ItemTypeMap.find(Type) == ItemTypeMap.end())
+        {
+            MissingTypes.push_back(Type);
+        }
+    }
+
+    // If any missing elements occur, this function returns true
+    if (MissingItems.size() || MissingTypes.size())
+    {
+        return true;
+    }
+    return false;
+}
